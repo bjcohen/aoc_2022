4073e9a21bb2a6b16520cafe0f876c80b1e3f7ad
Ben Cohen <ben@cohen-family.org>
2022-12-06 21:28:13 -0500

    day6

---
+++ src/day6.rs
@@ -0,0 +1,24 @@
+use anyhow::Result;
+use itertools::Itertools;
+use std::fs::read_to_string;
+
+fn main() -> Result<()> {
+    let mut buf = read_to_string("input6.txt")?;
+    buf.pop();
+
+    let vec = buf.chars().collect::<Vec<char>>();
+    for (i, win) in vec.windows(4).enumerate() {
+        if win.into_iter().unique().count() == 4 {
+            println!("{}", i+4);
+            break
+        }
+    }
+    for (i, win) in vec.windows(14).enumerate() {
+        if win.into_iter().unique().count() == 14 {
+            println!("{}", i+14);
+            break
+        }
+    }
+
+    Ok(())
+}
